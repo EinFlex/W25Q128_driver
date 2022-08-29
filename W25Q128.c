622ef7ed8708ff9c543bc354ccbc8d7f6d6a0a98
EinFlex <d.kerschat@gmx.net>
2022-08-29 11:25:09 +0200

    driver updated, currently used driver for development, STM32 HAL and LL, DMA Read / Write

--- W25Q128.c
+++ W25Q128.c
@@ -1,60 +1,356 @@
-#include "W25Q128.h"
+/*
+ * W25Q128JV.c
+ *
+ *  Created on: 12 Aug 2022
+ *      Author: Dominik Kerschat
+ */
+#include "W25Q128JV.h"
+#include "stm32f4xx_hal.h"
 
-// Initialize the Flash Memory, check WHO AM I
-void W25_Flash_Init(W25Q128_FLASH_TypeDef * hflash)
+uint8_t FLASHMEM_STRUCT_INIT(FLASHMEM *memory, GPIO_TypeDef *csport, uint16_t cspin, SPI_HandleTypeDef *spibus)
 {
-    // to get Manuf/Device ID
-    // 90h, Dummy x2, 00h, MF Byte, ID Byte
-    uint8_t datatx[] = {0x90, 0x00,0x00,0x00,0x00,0x00};
-    uint8_t datarx[] = {0x00,0x00};
-    HAL_GPIO_WritePin(hflash->CS_pin, GPIO_Set);
-    if (HAL_SPI_TransmitReceive(hflash->hspi, datatx, datarx, 6, 100) != HAL_OK)
-    {
-        HAL_GPIO_WritePin(hflash->CS_pin, GPIO_Reset);
-        hflash->status=W25_NOK;
-        return;
-    }
-    if (datarx[0] == 0xEF && datarx[1] == 0x17)
-    {
-        // Flash confirmed
-        // to read unique ID
-        // 4Bh, Dummyx4, UID63-0
-        datatx = {0x4B, 0x00,0x00,0x00,0x00, 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
-        datarx = uint8_t[8];
-        if (HAL_SPI_TransmitReceive(hflash->hspi, datatx, datarx,13,100) != HAL_OK)
-        {
-            HAL_GPIO_WritePin(hflash->CS_pin, GPIO_Reset);
-            hflash->status=W25_NOK;
-            return;
-        }
-        else
-        {
-            HAL_GPIO_WritePin(hflash->CS_pin, GPIO_Reset);
-            hflash->uniqueID = datarx;
-            hflas->status=W25_InitComplete;
-        }
-    }
-    else
-    {
-        HAL_GPIO_WritePin(hflash->CS_pin, GPIO_Reset);
-        hflash->status=W25_NOK;
-        return;
-    }
+	memory->csPort = csport;
+	memory->csPin = cspin;
+	memory->spiHandle = spibus;
+	memory->dma_transfer = flashmem_none;
+	memory->dma_length = 0;
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, SET); // write the cs pin high (inactive)
+	return 1;
 }
 
-void W25_Flash_ReadStatus(W25Q128_FLASH_TypeDef * hflash)
+uint8_t FLASHMEM_READ_UNIQUEID(FLASHMEM *memory)
 {
-    // read all staus registers
-    HAL_GPIO_WritePin(hflash->CS_pin, GPIO_Set);
-    uint8_t command = 0x05;
-    uint8_t[8] status1;
-    uint8_t[8] status2;
-    uint8_t[8] status3;
-    HAL_SPI_TransmitReceive(hflash->hspi, command, status1, 1, 100);
-    command = 0x35;
-    HAL_SPI_TransmitReceive(hflash->hspi, command, status2, 1, 100);
-    command = 0x15;
-    HAL_SPI_TransmitReceive(hflash->hspi, command, status3, 1, 100);
-    HAL_GPIO_WritePin(hflash->CS_pin, GPIO_Reset);
-    // WIP populate flash typedef with status register info
+	uint8_t txdata[5] = {SPIFLASH_UNIQUEID,0x00,0x00,0x00,0x00};
+	// sending the unique ID command plus 4 dummy bytes
+	// read unique ID directly to the memory typdef struct
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, RESET); // set cs pin active
+	uint8_t rxbuffer[13];
+	if (HAL_SPI_TransmitReceive(memory->spiHandle,txdata, rxbuffer,13, HAL_MAX_DELAY) == HAL_OK)
+	{
+		HAL_GPIO_WritePin(memory->csPort, memory->csPin, SET);
+		for (int i = 0; i < 8; i++)
+		{
+			memory->uniqueID[i] = rxbuffer[i+5];
+		}
+		return 1;
+	}
+	else
+		HAL_GPIO_WritePin(memory->csPort, memory->csPin, SET);
+	return 0;
+}
+
+uint8_t FLASHMEM_POWER_UP(FLASHMEM *memory) // release the memory from power down mode
+{
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, RESET);
+	uint8_t txcmd = SPIFLASH_RELEASE_POWERDOWN;
+	uint8_t ret = 0;
+	if (HAL_SPI_Transmit(memory->spiHandle,&txcmd, 1, HAL_MAX_DELAY) == HAL_OK)
+		ret = 1;
+	HAL_Delay(1);
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, SET);
+	return ret;
+}
+
+uint8_t FLASHMEM_POWER_UP_DEVID(FLASHMEM * memory, uint8_t *devid)
+{
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, RESET);
+	uint8_t txcmd[4] = {SPIFLASH_RELEASE_POWERDOWN, 0x00,0x00,0x00};
+	uint8_t ret = 0;
+	uint8_t rxbuffer[5];
+	if (HAL_SPI_TransmitReceive(memory->spiHandle, txcmd, rxbuffer, 5, HAL_MAX_DELAY) == HAL_OK)
+		ret = 1;
+	HAL_Delay(1);
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, SET);
+	devid[0] = rxbuffer[4];
+	return ret;
+}
+
+uint8_t FLASHMEM_POWER_DOWN(FLASHMEM *memory) // set memory to power down mode
+{
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, RESET);
+	uint8_t txcmd = SPIFLASH_POWERDOWN;
+	uint8_t ret = 0;
+	if(HAL_SPI_Transmit(memory->spiHandle,&txcmd, 1, HAL_MAX_DELAY) == HAL_OK)
+		ret = 1;
+	HAL_Delay(1);
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, SET);
+	return ret;
+}
+
+uint8_t FLASHMEM_READ_JEDECID(FLASHMEM *memory, uint8_t *data)
+{
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, RESET);
+	uint8_t txcmd = SPIFLASH_JEDECID;
+	uint8_t ret = 0;
+	uint8_t rxbuffer[4];
+	if (HAL_SPI_TransmitReceive(memory->spiHandle, &txcmd, rxbuffer, 4, HAL_MAX_DELAY) == HAL_OK)
+		ret = 1;
+	HAL_Delay(1);
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, SET);
+	for (int i = 0; i < 3; i++)
+	{
+		data[i] = rxbuffer[i+1];
+	}
+	return ret;
+}
+
+uint8_t FLASHMEM_READ_STATUSREG1(FLASHMEM *memory, uint8_t *data) // read the statusregister 1 content
+{
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, RESET);
+	uint8_t txcmd = SPIFLASH_READSR1;
+	uint8_t ret = 0;
+	uint8_t rxbuffer[2];
+	if (HAL_SPI_TransmitReceive(memory->spiHandle, &txcmd, rxbuffer, 2, HAL_MAX_DELAY) == HAL_OK)
+		ret = 1;
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, SET);
+	data[0] = rxbuffer[1];
+	return ret;
+}
+
+uint8_t FLASHMEM_WRITE_STATUSREG1(FLASHMEM *memory, uint8_t settings) // write settings byte to status register
+{
+	uint8_t ret = FLASHMEM_WRITE_ENABLE(memory); // enable write operations / disable is done atomatically
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, RESET);
+	// writeable bits in SR1: SEC, TB, BP [6:2]
+	// SEC: 0 -> protect 64KB blocks, 1 -> protect 4KB Sectors
+	// TB: 0 -> protect blocks from TOP, 1 -> protect blocks form bottom
+	// BP2,BP1,BP0: Define whic blocks to protect (see Memory Protection Table)
+	settings &= 0x7C; // only use the bits that can be written
+	uint8_t txcmd[2] = {SPIFLASH_WRITESR1, settings};
+	if (HAL_SPI_Transmit(memory->spiHandle, txcmd, 2, HAL_MAX_DELAY) == HAL_OK)
+		ret = 1;
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, SET);
+	return ret;
+}
+
+uint8_t FLASHMEM_WRITE_ENABLE(FLASHMEM *memory) // send write enable instruction
+{
+	uint8_t ret = 0;
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, RESET);
+	uint8_t txcmd = SPIFLASH_WRITE_ENABLE;
+	if (HAL_SPI_Transmit(memory->spiHandle, &txcmd, 1, HAL_MAX_DELAY) == HAL_OK)
+		ret = 1;
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, SET);
+	return ret;
+}
+
+uint8_t FLASHMEM_WRITE_DISABLE(FLASHMEM *memory)
+{
+	uint8_t ret = 0;
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, RESET);
+	uint8_t txcmd = SPIFLASH_WRITE_DISABLE;
+	if (HAL_SPI_Transmit(memory->spiHandle, &txcmd, 1, HAL_MAX_DELAY) == HAL_OK)
+		ret = 1;
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, SET);
+	return ret;
+}
+
+uint8_t FLASHMEM_WRITE_VOL_ENABLE(FLASHMEM *memory) // enables writing to the volatile status registers
+{
+	uint8_t ret = 0;
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, RESET);
+	uint8_t txcmd = SPIFLASH_VOLSR_ENABLE;
+	if (HAL_SPI_Transmit(memory->spiHandle, &txcmd, 1, HAL_MAX_DELAY) == HAL_OK)
+		ret = 1;
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, SET);
+	return ret;
+}
+
+uint8_t FLASHMEM_READ_STATUSREG2(FLASHMEM *memory, uint8_t *data)
+{
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, RESET);
+	uint8_t txcmd = SPIFLASH_READSR2;
+	uint8_t ret = 0;
+	uint8_t rxbuffer[2];
+	if (HAL_SPI_TransmitReceive(memory->spiHandle, &txcmd, rxbuffer, 2, HAL_MAX_DELAY) == HAL_OK)
+		ret = 1;
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, SET);
+	data[0] = rxbuffer[1];
+	return ret;
+}
+
+uint8_t FLASHMEM_WRITE_STATUSREG2(FLASHMEM *memory, uint8_t settings)
+{
+	uint8_t ret = FLASHMEM_WRITE_ENABLE(memory); // enable write operations
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, RESET);
+	// writeable bits in SR2:
+	// CMP:
+	// QE:
+	// LB3,LB2,LB1
+	// SRL:
+	//settings &= 0x7B; // only use the bits that can be written
+	settings &= 0x43; // no security bits can be overwritten @WIP for DEV only
+	uint8_t txcmd[2] = {SPIFLASH_WRITESR2, settings};
+	if (HAL_SPI_Transmit(memory->spiHandle, txcmd, 2, HAL_MAX_DELAY) == HAL_OK)
+		ret = 1;
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, SET);
+	return ret;
+}
+
+uint8_t FLASHMEM_READ_STATUSREG3(FLASHMEM *memory, uint8_t *data)
+{
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, RESET);
+	uint8_t txcmd = SPIFLASH_READSR3;
+	uint8_t ret = 0;
+	uint8_t rxbuffer[2];
+	if (HAL_SPI_TransmitReceive(memory->spiHandle, &txcmd, rxbuffer, 2, HAL_MAX_DELAY) == HAL_OK)
+		ret = 1;
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, SET);
+	data[0] = rxbuffer[1];
+	return ret;
+}
+
+uint8_t FLASHMEM_WRITE_STATUSREG3(FLASHMEM *memory, uint8_t settings)
+{
+	uint8_t ret = FLASHMEM_WRITE_ENABLE(memory); // enable write operations
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, RESET);
+	// writeable bits in SR3:
+	// HOLD/RST:
+	// DRV1:
+	// DRV0,LB2,LB1:
+	// WPS:
+	settings &= 0xE4; // no security bits can be overwritten @WIP for DEV only
+	uint8_t txcmd[2] = {SPIFLASH_WRITESR3, settings};
+	if (HAL_SPI_Transmit(memory->spiHandle, txcmd, 2, HAL_MAX_DELAY) == HAL_OK)
+		ret = 1;
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, SET);
+	return ret;
+}
+
+uint8_t FLASHMEM_READ_DATA(FLASHMEM *memory, uint8_t *address, uint8_t *data, uint16_t length)
+{
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, RESET);
+	uint8_t txcmd[4] = {SPIFLASH_READDATA, address[0],address[1], address[2]};
+	uint8_t ret = 0;
+	uint8_t rxbuffer[4+length];
+	if (HAL_SPI_TransmitReceive(memory->spiHandle, txcmd, rxbuffer, (4 + length), HAL_MAX_DELAY) == HAL_OK)
+		ret = 1;
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, SET);
+	for(int i = 0; i < length; i++)
+	{
+		data[i] = rxbuffer[i+4];
+	}
+	return ret;
+}
+
+uint8_t FLASHMEM_WRITE_DATA(FLASHMEM *memory, uint8_t *address, uint8_t *data, uint16_t length)
+{
+	FLASHMEM_WRITE_ENABLE(memory);
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, RESET);
+	uint8_t txcmd[4+length];
+	txcmd[0] = SPIFLASH_PAGEPROGRAM, txcmd[1] = address[0], txcmd[2] = address[1], txcmd[3] = address[2];
+	for (int i = 0; i < length; i++)
+	{
+		txcmd[i+4] = data[i];
+	}
+	uint8_t ret = 0;
+	if (HAL_SPI_Transmit(memory->spiHandle, txcmd, (4 + length), HAL_MAX_DELAY) == HAL_OK)
+		ret = 1;
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, SET);
+	return ret;
+}
+
+uint8_t FLASHMEM_ERASE_SECTOR(FLASHMEM *memory, uint8_t *address)
+{
+	uint8_t ret = FLASHMEM_WRITE_ENABLE(memory); // enable write operations
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, RESET);
+	uint8_t txcmd[4];
+		txcmd[0] = SPIFLASH_SECTORERASE, txcmd[1] = address[0], txcmd[2] = address[1], txcmd[3] = address[2];
+	if (HAL_SPI_Transmit(memory->spiHandle, txcmd, 2, HAL_MAX_DELAY) == HAL_OK)
+		ret = 1;
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, SET);
+	return ret;
+}
+
+uint8_t FLASHMEM_GLOBAL_BLOCKLOCK(FLASHMEM *memory) // lock all blocks
+{
+	uint8_t ret = 0;
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, RESET);
+	uint8_t txcmd = SPIFLASH_GLOBALBLOCKLOCK;
+	if (HAL_SPI_Transmit(memory->spiHandle, &txcmd, 1, HAL_MAX_DELAY) == HAL_OK)
+		ret = 1;
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, SET);
+	return ret;
+}
+
+uint8_t FLASHMEM_GLOBAL_BLOCKUNLOCK(FLASHMEM *memory) /// unlock all blocks
+{
+	uint8_t ret = 0;
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, RESET);
+	uint8_t txcmd = SPIFLASH_GLOBALBLOCKUNLOCK;
+	if (HAL_SPI_Transmit(memory->spiHandle, &txcmd, 1, HAL_MAX_DELAY) == HAL_OK)
+		ret = 1;
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, SET);
+	return ret;
+}
+
+uint8_t FLASHMEM_INDIVIDUAL_BLOCKLOCK(FLASHMEM *memory, uint8_t *address) // lock indivudual blocks
+{
+	uint8_t ret = 0;
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, RESET);
+	uint8_t txcmd[4] = {SPIFLASH_INDIVBLOCKLOCK, address[0],address[1],address[2]};
+	if (HAL_SPI_Transmit(memory->spiHandle, txcmd, 4, HAL_MAX_DELAY) == HAL_OK)
+		ret = 1;
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, SET);
+	return ret;
+}
+
+uint8_t FLASHMEM_INDIVIDUAL_BLOCKUNLOCK(FLASHMEM *memory, uint8_t *address) // unlock individual blocks
+{
+	uint8_t ret = 0;
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, RESET);
+	uint8_t txcmd[4] = {SPIFLASH_INDIVBLOCKUNLOCK, address[0],address[1],address[2]};
+	if (HAL_SPI_Transmit(memory->spiHandle, txcmd, 4, HAL_MAX_DELAY) == HAL_OK)
+		ret = 1;
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, SET);
+	return ret;
+}
+
+uint8_t FLASHMEM_DMA_READ_DATA(FLASHMEM *memory, uint8_t *address, uint8_t *databuffer, uint16_t length)
+{
+	uint8_t ret = 0;
+	if (memory->dma_transfer == flashmem_none)
+	{
+		memory->dma_length = length;
+		HAL_GPIO_WritePin(memory->csPort, memory->csPin, RESET);
+		uint8_t txcmd[4] = {SPIFLASH_READDATA, address[0],address[1], address[2]};
+		memory->dma_transfer = flashmem_dmaread;
+		if (HAL_SPI_TransmitReceive_DMA(memory->spiHandle, txcmd, databuffer, 4+length) == HAL_OK)
+			ret = 1;
+	}
+	return ret;
+}
+
+uint8_t FLASHMEM_DMA_WRITE_DATA(FLASHMEM *memory, uint8_t *address, uint8_t *data, uint16_t length)
+{
+	uint8_t ret = 0;
+	if (memory->dma_transfer == flashmem_none)
+	{
+		HAL_GPIO_WritePin(memory->csPort, memory->csPin, RESET);
+		uint8_t txcmd[4+length];
+		txcmd[0] = SPIFLASH_PAGEPROGRAM, txcmd[1] = address[0], txcmd[2] = address[1], txcmd[3] = address[2];
+		memory->dma_transfer = flashmem_dmawrite;
+		for (int i = 0; i < length; i++)
+		{
+			txcmd[i+4] = data[i];
+		}
+		if (HAL_SPI_Transmit_DMA(memory->spiHandle, txcmd, 4+length) == HAL_OK)
+			ret = 1;
+	}
+	return ret;
+}
+
+uint8_t FLASHMEM_DMA_COMPLETE(FLASHMEM *memory, uint8_t *databuffer)
+{
+	HAL_GPIO_WritePin(memory->csPort, memory->csPin, SET);
+	if (memory->dma_transfer == flashmem_dmaread)
+	{
+		for(int i = 0; i < memory->dma_length; i++)
+		{
+			databuffer[i] = databuffer[i+4];
+		}
+	}
+	memory->dma_transfer = flashmem_none;
+	memory->dma_length = 0;
+	return 1;
 }
--- W25Q128.h
+++ W25Q128.h
@@ -1,25 +1,95 @@
-#ifndef W25Q128
-#define W25Q128
+/*
+ * W25Q128JV.h
+ *
+ *  Created on: 12 Aug 2022
+ *      Author: Dominik Kerschat
+ */
 
-typedef struct{
-    SPI_HandleTypeDef * hspi;
-    uint16_t CS_pin;
-    uint16_t WP_pin; // IO2
-    uint16_t HR_pin; // IO3 | Hold or Reset Pin (active low)
-    uint8_t enableQSpi; // weather Quad SPI should be enabled or disabled on the next Init
-    uint8_t [] uniqueID; // array to store the unique id
-    W25_StatusTypeDef status;
-}W25Q128_FLASH_TypeDef;
+#ifndef W25Q128JV_W25Q128JV_H_
+#define W25Q128JV_W25Q128JV_H_
+#include "stm32f4xx_hal.h"
 
-typedef enum{
-    W25_InitComplete;
-    W25_SWProtected; // when SW write protection is active
-    W25_HWProtected; // when HW write protection is active (SW protection could also be active)
-    W25_NOK; // any failure
-    W25_Writing; // when a erase/write is in progress
-    W25_WEL; // when the Write enable latch is active but no write is in progress
-}W25_StatusTypeDef;
+// instructions table (standard SPI instructions)
+#define SPIFLASH_WRITE_ENABLE 0x06 // write enable
+#define SPIFLASH_VOLSR_ENABLE 0x50 // volatile SR write enable
+#define SPIFLASH_WRITE_DISABLE 0x04 // write disable
+#define SPIFLASH_RELEASE_POWERDOWN 0xAB // release power down followed by dummy bytes and the device ID
+#define SPIFLASH_MFANDDEVID 0x90 // dummy bytes followed by 0x00, the manufacturer ID and device ID
+#define SPIFLASH_JEDECID 0x9F // MF, JEDEC and DEVICE ID
+#define SPIFLASH_UNIQUEID 0x4B // dummy bytes followed by the unique ID
+#define SPIFLASH_READDATA 0x03 // read data followed by memory address bytes, flash respons with data
+#define SPIFLASH_FASTREAD 0x0B // read multiple data bytes: address bytes, dummy byte, falsh respons with data until /CS is inavtice again
+#define SPIFLASH_PAGEPROGRAM 0x02 // write data to a page with a starting address
+#define SPIFLASH_SECTORERASE 0x20 // 4KB
+#define SPIFALSH_BLOCKERASE32 0x52 // 32KB
+#define SPIFLASH_BLOCKERASE64 0xD8 // 64KB
+#define SPIFLASH_CHIPERASE 0xC7 // or 0x60 ?
+#define SPIFLASH_READSR1 0x05 // read status register 1
+#define SPIFLASH_WRITESR1 0x01 // write status register 1 (can be used to write to SR1&SR2)
+#define SPIFLASH_READSR2 0x35 // read status register 2
+#define SPIFLASH_WRITESR2 0x32 // write status register 2
+#define SPIFLASH_READSR3 0x15 // read status register 3
+#define SPIFLASH_WRITESR3 0x11 // write status register 3
+#define SPIFLASH_READSFDPREG 0x5A // read SFDP register
+#define SPIFLASH_ERASESECREG 0x44 // erase security register
+#define SPIFLASH_PROGSECREG 0x42 // program security register
+#define SPIFLASH_READSECREG 0x48 // read security register
+#define SPIFLASH_GLOBALBLOCKLOCK 0x7E // lock all blocks
+#define SPIFLASH_GLOBALBLOCKUNLOCK 0x98 // unlock all blocks
+#define SPIFLASH_READBLOCKLOCK 0x3D // read block lock from address
+#define SPIFLASH_INDIVBLOCKLOCK 0x36 // lock individual block by address
+#define SPIFLASH_INDIVBLOCKUNLOCK 0x39 // unlock individual block by addess
+#define SPIFLASH_ERASEPROGSUSPEND 0x75 // suspend all program or erase operations
+#define SPIFLASH_ERASEPROGRESUME 0x7A // resume all program or erase operations
+#define SPIFLASH_POWERDOWN 0xB9 // power down flash memory (has to be released before use again)
+#define SPIFLASH_ENABLERESET 0x66 // enable a device reset
+#define SPIFLASH_RESETDEVICE 0x99 // reset the device (has to be enabled)
 
-void W25_Flash_Init(W25Q128_FLASH_TypeDef * hflash);
-void W25_Flash_ReadStatus(W25Q128_FLASH_TypeDef * hflash);
-#endif
+typedef struct
+{
+	uint8_t isBusy; // indicates if there is currently a transfer ongoing
+	GPIO_TypeDef *csPort; // the gpio port of the cs pin
+	uint16_t csPin; // the cs pin
+	uint8_t uniqueID[8]; // the devices unique id is stored here
+	SPI_HandleTypeDef *spiHandle; // the SPI bus connected to the memory
+	uint8_t dma_transfer;
+	uint16_t dma_length; // the length of the last dma read operation
+} FLASHMEM;
+
+enum {flashmem_none = 0, flashmem_dmaread = 1, flashmem_dmawrite = 2};
+
+// device management functions
+uint8_t FLASHMEM_STRUCT_INIT(FLASHMEM *memory, GPIO_TypeDef *csport, uint16_t cspin, SPI_HandleTypeDef *spibus);
+uint8_t FLASHMEM_READ_UNIQUEID(FLASHMEM *memory);
+uint8_t FLASHMEM_POWER_UP(FLASHMEM *memory); // release the memory from power down mode
+uint8_t FLASHMEM_POWER_UP_DEVID(FLASHMEM *memory, uint8_t *devid); // release the memory from power down and read device id
+uint8_t FLASHMEM_POWER_DOWN(FLASHMEM *memory); // set memory to power down mode
+uint8_t FLASHMEM_READ_JEDECID(FLASHMEM *memory, uint8_t *data); // read the jedec ID of the memory
+
+// staus register managment
+uint8_t FLASHMEM_READ_STATUSREG1(FLASHMEM *memory, uint8_t *data); // read the statusregister 1 content
+uint8_t FLASHMEM_WRITE_STATUSREG1(FLASHMEM *memory, uint8_t settings); // write settings byte to status register
+uint8_t FLASHMEM_WRITE_ENABLE(FLASHMEM *memory); // send write enable instruction
+uint8_t FLASHMEM_WRITE_DISABLE(FLASHMEM *memory); // send write disable instruction
+uint8_t FLASHMEM_READ_STATUSREG2(FLASHMEM *memory, uint8_t *data);
+uint8_t FLASHMEM_WRITE_STATUSREG2(FLASHMEM *memory, uint8_t settings); // this register contains security register settings that can only be programmed once !!!
+uint8_t FLASHMEM_READ_STATUSREG3(FLASHMEM *memory, uint8_t *data);
+uint8_t FLASHMEM_WRITE_STATUSREG3(FLASHMEM *memory, uint8_t settings);
+uint8_t FLASHMEM_WRITE_VOL_ENABLE(FLASHMEM *memory); // enables writing to the volatile registers without degrading the non-volatile write performance
+
+
+// Data transfer functions
+uint8_t FLASHMEM_READ_DATA(FLASHMEM *memory, uint8_t *address, uint8_t *data, uint16_t length);
+uint8_t FLASHMEM_WRITE_DATA(FLASHMEM *memory, uint8_t *address, uint8_t *data, uint16_t length);
+uint8_t FLASHMEM_ERASE_DATA(FLASHMEM *memory, uint8_t *address);
+uint8_t FLASHMEM_GLOBAL_BLOCKLOCK(FLASHMEM *memory); // lock all blocks
+uint8_t FLASHMEM_GLOBAL_BLOCKUNLOCK(FLASHMEM *memory); /// unlock all blocks
+uint8_t FLASHMEM_INDIVIDUAL_BLOCKLOCK(FLASHMEM *memory, uint8_t *address); // lock indivudual blocks
+uint8_t FLASHMEM_INDIVIDUAL_BLOCKUNLOCK(FLASHMEM *memory, uint8_t *address); // unlock individual blocks
+uint8_t FLASHMEM_DMA_READ_DATA(FLASHMEM *memory, uint8_t *address, uint8_t *databuffer, uint16_t length);
+uint8_t FLASHMEM_DMA_WRITE_DATA(FLASHMEM *memory, uint8_t *address, uint8_t *data, uint16_t length);
+
+uint8_t FLASHMEM_DMA_COMPLETE(FLASHMEM *memory, uint8_t *databuffer); //DMA Complete function has to be called by the DMA TxRx Complete and Tx Compelte Callbacks
+
+#endif /* W25Q128JV_W25Q128JV_H_ */
+
